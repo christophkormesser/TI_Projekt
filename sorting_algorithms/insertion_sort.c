2ada80116c873fb4b2bd3e631bb2a45eac0294dc
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-01-06 13:01:12 +0100

    added Clion project files & folders, and insertion sort files

---
+++ sorting_algorithms/insertion_sort.c
@@ -0,0 +1,5 @@
+//
+// Created by Christoph Kormesser on 05.01.22.
+//
+
+#include "insertion_sort.h"
---
+++ sorting_algorithms/insertion_sort.h
@@ -0,0 +1,8 @@
+//
+// Created by Christoph Kormesser on 05.01.22.
+//
+
+#ifndef TI_PROJEKT_INSERTION_SORT_H
+#define TI_PROJEKT_INSERTION_SORT_H
+
+#endif //TI_PROJEKT_INSERTION_SORT_H
