a4099e1971e7b932f5b53083b338c0b750ed6f82
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-01-06 13:42:47 +0100

    forgot to add .gitignore

---
+++ shared/create_array.c
@@ -0,0 +1,5 @@
+//
+// Created by Christoph Kormesser on 06.01.22.
+//
+
+#include "create_array.h"
---
+++ shared/create_array.h
@@ -0,0 +1,8 @@
+//
+// Created by Christoph Kormesser on 06.01.22.
+//
+
+#ifndef TI_PROJEKT_CREATE_ARRAY_H
+#define TI_PROJEKT_CREATE_ARRAY_H
+
+#endif //TI_PROJEKT_CREATE_ARRAY_H
