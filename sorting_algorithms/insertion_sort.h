1309465ef9e173fcdb91f63723b7a11a60874659
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-01-19 20:52:38 +0100

    added finished insertion_sort.c

--- sorting_algorithms/insertion_sort.h
+++ sorting_algorithms/insertion_sort.h
@@ -5,6 +5,11 @@
 #ifndef TI_PROJEKT_INSERTION_SORT_H
 #define TI_PROJEKT_INSERTION_SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+
 #include "../shared/copy_array.h"
 
 int insertion_sort(short *array);
