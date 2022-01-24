f96ad02a090f24947817d1df20753019af36b9a8
neydineyd <github@juneydahmad.at>
2022-01-24 18:03:34 +0100

    BubSort_1803_24012022

--- main.c
+++ main.c
@@ -24,12 +24,16 @@ int main(void) {
     short *short_array = create_array(8, min, max, RANDOM);
     short *medium_array = create_array(16, min, max, RANDOM);
     short *large_array = create_array(64, min, max, RANDOM);
-    short *bub2k_array = create_array(2000, min, max, RANDOM);
+    //short *bub2k_array = create_array(2000, min, max, RANDOM);
 
     double ins_runtime_small = insertion_sort(short_array, 8);
     double ins_runtime_medium = insertion_sort(medium_array, 16);
     double ins_runtime_large = insertion_sort(large_array, 64);
 
+    // BUBBLE SORT:
+    double bub_runtime_small = bubble_sort(short_array, 8);
+    double bub_runtime_medium = bubble_sort(medium_array, 16);
+    double bub_runtime_large = bubble_sort(large_array, 64);
     //printf("\n\n1. Bubble Sort - [8]\n");
     //bubble_sort(large_array);
 
@@ -48,13 +52,15 @@ int main(void) {
     free(short_array);
     free(medium_array);
     free(large_array);
-    free(bub2k_array);
+    //free(bub2k_array);
 
     printf("\n\nRUNTIMES\n------\n");
     printf("Insertion Sort small: %lf secs\n", ins_runtime_small);
     printf("Insertion Sort medium: %lf secs\n", ins_runtime_medium);
     printf("Insertion Sort large: %lf secs\n", ins_runtime_large);
-    printf("Bubble Sort: \n");
+    printf("Bubble Sort small: %lf secs\n", bub_runtime_small);
+    printf("Bubble Sort medium: %lf secs\n", bub_runtime_medium);
+    printf("Bubble Sort large: %lf secs\n", bub_runtime_large);
     printf("Merge Sort: \n");
     printf("Quick Sort: \n");
 
