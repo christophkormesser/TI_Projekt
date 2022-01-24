ee07eaa45fc8466466ccb90f13784400afb5c7e7
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-01-24 12:01:37 +0100

    removed "//"

--- main.c
+++ main.c
@@ -21,33 +21,32 @@ int main(void) {
     int min = -32767;
     int max = 32767;
 
-    short *short_array = create_array(16, min, max, DSC);
-    // short *medium_array = create_array(8, min, max, DSC);
-    // short *large_array = create_array(64, min, max, RANDOM);
-    // short *bub2k_array = create_array(2000, min, max, RANDOM);
+    short *short_array = create_array(8, min, max, ASC);
+    short *medium_array = create_array(8, min, max, DSC);
+    short *large_array = create_array(64, min, max, RANDOM);
+    short *bub2k_array = create_array(2000, min, max, RANDOM);
 
-    //insertion_sort(short_array);
     double ins_runtime = insertion_sort(short_array, 8);
 
     printf("\n\n1. Bubble Sort - [8]\n");
-    // bubble_sort(large_array);
+    bubble_sort(large_array);
 
-    // merge_sort(short_array, 64);
+    merge_sort(short_array, 64);
 /*
     printf("2. Bubble Sort - [16]\n");
     bubble_sort(medium_array, 16);
     printf("3. Bubble Sort - [64]\n");
     bubble_sort(large_array, 64);
 */
-   //bubble_sort_2k(bub2k_array, 2000);
+   bubble_sort_2k(bub2k_array, 2000);
 
 
-	//quick_sort(short_array, 8);
+	quick_sort(short_array, 8);
 
     free(short_array);
-    // free(medium_array);
-    // free(large_array);
-    // free(bub2k_array);
+    free(medium_array);
+    free(large_array);
+    free(bub2k_array);
 
     printf("\nRUNTIMES\n------\n");
     printf("Insertion Sort: %lf secs\n", ins_runtime);
